eec05c72b38d303220b8989e0a2effa3f41fc7d4
Sebastian <jsortiz82@misena.edu.co>
2024-09-20 21:38:43 -0500

    se sube nuevo mensaje

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const surpriseMessage = document.getElementById('surprise-message');
-    const card = document.querySelector('.card');
+const envelope = document.querySelector('.envelope');
+const openLetterBtn = document.getElementById('openLetterBtn');
 
-    // Mostrar carta después de que el panda y el corazón lleguen al centro
-    setTimeout(() => {
-        card.style.opacity = 1; // Hacer visible la carta
-        card.style.animation = "appear 1s forwards"; // Aplicar animación
-        surpriseMessage.textContent = '¡El panda te envía mucho amor y amistad! 💖';
-        surpriseMessage.style.opacity = 1;
-    }, 6000); // Después de 6 segundos
+openLetterBtn.addEventListener('click', () => {
+    envelope.classList.toggle('open');
 });
